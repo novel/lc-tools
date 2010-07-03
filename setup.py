9d0c8b8c8707f6aed1893822aac97bc5d23c6ebc
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-07-02 22:20:37 -0700

    Stupid novel forgot to add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name="lctools",
+        version="0.1.0",
+        author="Roman Bogorodskiy",
+        author_email="bogorodskiy@gmail.com",
+        url="http://github.com/novel/lc-tools",
+        packages=["lctools"],
+        scripts=["lc-drivers-list",
+            "lc-image-list",
+            "lc-node-add",
+            "lc-node-do",
+            "lc-node-list",
+            "lc-sizes-list"]
+)
