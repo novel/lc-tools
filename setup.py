017f1c5ce736db94d638e6e5d4c1faa4307e0885
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-07-15 09:58:35 -0700

    Add license and add classifiers for pypi.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="lctools",
-        version="0.1.0",
+        version="0.1.1",
         description="CLI tools for managing clouds, based on libcloud",
         author="Roman Bogorodskiy",
         author_email="bogorodskiy@gmail.com",
@@ -15,4 +15,13 @@ setup(name="lctools",
             "lc-node-add",
             "lc-node-do",
             "lc-node-list",
-            "lc-sizes-list"])
+            "lc-sizes-list"],
+        license='Apache License (2.0)',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: System'],)
