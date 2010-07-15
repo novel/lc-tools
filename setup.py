f4e3e7985bf3ce55dfeba2df471016baee37e2d0
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-07-15 09:06:30 -0700

    Add __init__.py for package to the repo and add description to setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ except ImportError:
 
 setup(name="lctools",
         version="0.1.0",
+        description="CLI tools for managing clouds, based on libcloud",
         author="Roman Bogorodskiy",
         author_email="bogorodskiy@gmail.com",
         url="http://github.com/novel/lc-tools",
