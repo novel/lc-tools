cab6f62e6c11376bef231a6fcd3499b812feb59c
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-07-20 11:33:55 -0700

    Add MANIFEST.in file to roll better sdist tarballs.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="lctools",
-        version="0.1.1",
+        version="0.1.2",
         description="CLI tools for managing clouds, based on libcloud",
         author="Roman Bogorodskiy",
         author_email="bogorodskiy@gmail.com",
