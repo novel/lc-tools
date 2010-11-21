6252b949ede4ab12e519644646dbd8197ffdd091
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-11-21 09:55:27 +0300

    Implement lc-rs-image-save -- a tool for creating custom rackspace images.

--- lctools/shortcuts.py
+++ lctools/shortcuts.py
@@ -8,6 +8,13 @@ from printer import Printer
 
 
 def lister_main(what):
+    """Shortcut for main() routine for lister
+    tools, e.g. lc-SOMETHING-list
+
+    @param what: what we are listing, e.g. 'nodes'
+    @type what: C{string}
+    """
+
     list_method = "list_%s" % what
     profile = "default"
     format = None
@@ -31,6 +38,42 @@ def lister_main(what):
             Printer.do(node, format)
     except Exception, err:
         sys.stderr.write("Error: %s\n" % str(err))
+
+def save_image_main():
+    """Shortcut for main() routine for provider
+    specific image save tools.
+    """
+
+    def usage(progname):
+        sys.stdout.write("%s -i <node_id> -n <image_name> [-p <profile]\n\n" % progname)
+    
+    profile = 'default'
+    node_id = None
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:n:p:")
+    except getopt.GetoptError, err:
+        sys.stderr.write("%s\n" % str(err))
+        sys.exit(1)
+
+    for o, a in opts:
+        if o == "-i":
+            node_id = a
+        if o == "-n":
+            name = a
+        if o == "-p":
+            profile = a
+
+    if node_id is None or name is None:
+        usage(sys.argv[0])
+        sys.exit(1)
+
+    conn = get_lc(profile)
+
+    node = get_node_or_fail(conn, node_id, print_error_and_exit, 
+            ("Error: cannot find node with id '%s'." % node_id,))
+
+    Printer.do(conn.ex_save_image(node, name))
 
 def get_node_or_fail(conn, node_id, coroutine=None, cargs=(), ckwargs={}):
     """Shortcut to get a single node by its id. In case when 
